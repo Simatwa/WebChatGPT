99f9544b88fd227a84b2d798276559e6536893e7
Simatwa <smartwacaleb@gmail.com>
2024-02-21 13:51:38 +0300

    Update dependencies

--- WebChatGPT/__init__.py
+++ WebChatGPT/__init__.py
@@ -4,9 +4,9 @@ from importlib import metadata
 __all__ = ["ChatGPT"]
 
 try:
-    __version__ = metadata.version('webchatgpt')
+    __version__ = metadata.version("webchatgpt")
 except metadata.PackageNotFoundError:
-    __version__='0.0.0'
+    __version__ = "0.0.0"
 __author__ = "Smartwa"
 __repo__ = "https://github.com/Simatwa/WebChatGPT"
 __info__ = "Reverse Engineering of ChatGPT Web-version."
